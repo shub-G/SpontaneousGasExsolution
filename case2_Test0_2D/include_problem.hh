c4741f02962273e41e71d3ecab306ce9f3c1cf51
Shubhangi Gupta <sgupta@geomar.de>
2022-03-28 13:37:47 +0000

    Upload New File

---
+++ case2_Test0_2D/include_problem.hh
@@ -0,0 +1,27 @@
+#ifndef CASE2_TEST0_2D_INCLUDE_PROBLEM_HH_
+#define CASE2_TEST0_2D_INCLUDE_PROBLEM_HH_
+
+#include "../extras/ParameterTraits.hh"
+#include "../extras/Evaluation.hh"
+
+#include "../operators/indices.hh"
+
+/*******************************************/
+// PROBLEM SPECIFICATION
+#include"characteristic_values.hh"
+#include"grids/grid.hh"
+#include"parameters.hh"
+#include"properties/include_properties.hh"
+#include"initial_conditions.hh"
+#include"boundary_conditions.hh"
+/*******************************************/
+
+#include "../operators/projectionoperator.hh"
+#include "../operators/postprocess.hh"
+#include "../operators/initialvaluefunction.hh"
+#include "../operators/boundaryvaluefunction.hh"
+#include "../operators/localoperator.hh"
+#include "../operators/timeoperator.hh"
+#include "driver.hh"
+
+#endif /* CASE2_TEST0_2D_INCLUDE_PROBLEM_HH_ */
