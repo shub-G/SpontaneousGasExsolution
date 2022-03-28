678f6dad616d244d65671b6ceb3c94648b21f46e
Shubhangi Gupta <sgupta@geomar.de>
2022-03-28 13:25:11 +0000

    Upload New File

---
+++ case1_Test0_1D/include_problem.hh
@@ -0,0 +1,28 @@
+#ifndef CASE1_TEST0_1D_INCLUDE_PROBLEM_HH_
+#define CASE1_TEST0_1D_INCLUDE_PROBLEM_HH_
+
+#include "../extras/ParameterTraits.hh"
+#include "../extras/Evaluation.hh"
+
+#include "../operators/indices.hh"
+
+/*******************************************/
+// PROBLEM SPECIFICATION
+#include"characteristic_values.hh"
+#include"grids/grid.hh"
+#include"parameters.hh"
+#include"properties/include_properties.hh"
+#include"qoi.hh"
+#include"initial_conditions.hh"
+#include"boundary_conditions.hh"
+/*******************************************/
+
+#include "../operators/projectionoperator.hh"
+#include "../operators/postprocess.hh"
+#include "../operators/initialvaluefunction.hh"
+#include "../operators/boundaryvaluefunction.hh"
+#include "../operators/localoperator.hh"
+#include "../operators/timeoperator.hh"
+#include "driver.hh"
+
+#endif /* CASE1_TEST0_1D_INCLUDE_PROBLEM_HH_ */
